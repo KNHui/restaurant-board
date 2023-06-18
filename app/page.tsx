908155d6d45e973dbf66608131638ffbc7fa8622
NamHui Kim <rlasoaen@naver.com>
2023-06-18 18:38:34 +0900

    feat: Add layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,20 @@
 'use client';;
-import Link from 'next/link';
 import './page.module.css';
-import { StoreService } from './server/services/store-service';
-import BasicButtons from './components/buttons';
-import BasicButtonGroup from './components/button-group';
-import { Container, Grid } from '@mui/material';
-import CustomizedDialogs from './components/form-dialog';
+import { Container } from '@mui/material';
 
 export default function Home() {
-  const storeService = new StoreService();
-  const longString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
-
   return (
     <Container fixed>
-      <BasicButtons />
-      <hr />
-      <BasicButtonGroup />
-      <hr />
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <ul style={{ border: '1px solid gray' }}>
-            <li>
-              <Link href="/sub">/sub</Link>
-            </li>
-            <li>
-              <Link href="/sub/1">/sub/1</Link>
-            </li>
-            <li>
-              <Link href="/sub/2">/sub/2</Link>
-            </li>
-
-            <li>
-              <Link href="/sub/about">/sub/about</Link>
-            </li>
-          </ul>
-        </Grid>
-        <Grid item xs={8}>
-          <p style={{ border: '1px solid gray' }}>
-            {longString}
-          </p>
-        </Grid>
-      </Grid>
-      <hr />
-      <CustomizedDialogs />
+      <header>Main Header</header>
+      <div>
+        <header>Sub Header</header>
+        <main>
+          Body
+        </main>
+        <footer>
+          Footer
+        </footer>
+      </div>
     </Container>
   )
 }
