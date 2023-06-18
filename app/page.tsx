03c632b5091e97f313ea079c2067d9e6b100dba9
NamHui Kim <rlasoaen@naver.com>
2023-06-18 14:39:34 +0900

    feat: Add Grid

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import './page.module.css';
 import { StoreService } from './server/services/store-service';
 import BasicButtons from './components/buttons';
 import BasicButtonGroup from './components/button-group';
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 
 export default function Home() {
   const storeService = new StoreService();
@@ -16,23 +16,30 @@ export default function Home() {
       <hr></hr>
       <BasicButtonGroup></BasicButtonGroup>
       <hr></hr>
-      /page.tsx
-      <ul>
-        <li>
-          <Link href="/sub">/sub</Link>
-        </li>
-        <li>
-          <Link href="/sub/1">/sub/1</Link>
-        </li>
-        <li>
-          <Link href="/sub/2">/sub/2</Link>
-        </li>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <ul style={{ border: '1px solid gray' }}>
+            <li>
+              <Link href="/sub">/sub</Link>
+            </li>
+            <li>
+              <Link href="/sub/1">/sub/1</Link>
+            </li>
+            <li>
+              <Link href="/sub/2">/sub/2</Link>
+            </li>
 
-        <li>
-          <Link href="/sub/about">/sub/about</Link>
-        </li>
-      </ul>
-
+            <li>
+              <Link href="/sub/about">/sub/about</Link>
+            </li>
+          </ul>
+        </Grid>
+        <Grid item xs={8}>
+          <p style={{ border: '1px solid gray' }}>
+            {longString}
+          </p>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
